bfca681d8c2180e389d2a4996cd59595429db4f4
Kubilay Yazoğlu <kyazogluu@gmail.com>
2020-08-31 23:44:26 +0300

    Add files via upload

--- laptopPriceChecker.py
+++ laptopPriceChecker.py
@@ -3,83 +3,63 @@ from bs4 import BeautifulSoup
 import smtplib
 from email.message import EmailMessage
 
-laptop_links = ["https://www.epey.com/laptop/hp-probook-440-g6-7df56ea.html", \
-                "https://www.epey.com/laptop/hp-pavilion-x360-14-dh1005nt-8xh01ea.html", \
-                "https://www.epey.com/laptop/asus-um431da-am024t.html", \
-                "https://www.epey.com/laptop/hp-14-ce3003nt-3h905ea.html", \
-                "https://www.epey.com/laptop/acer-swift-3-sf314-58g-53hn.html", \
-                "https://www.epey.com/laptop/huawei-matebook-13-8plus512-gb.html", \
-                "https://www.epey.com/laptop/huawei-matebook-d-14-amd.html", \
-                "https://www.epey.com/laptop/lenovo-s540-81nd003utx.html", \
-                "https://www.epey.com/laptop/hp-14-cf2005nt-9cp83ea.html", \
-                "https://www.epey.com/laptop/hp-340s-g7-10r29ea.html", \
-                "https://www.epey.com/laptop/hp-14-cf2004nt-9cn08ea.html", \
-                "https://www.epey.com/laptop/hp-14-cf1018nt-8bw26ea.html", \
-                "https://www.epey.com/laptop/dell-inspiron-5490-s510f82n.html", \
-                "https://www.epey.com/laptop/monster-huma-h4-v3-2.html"] 
+product_links = ["https://www.epey.com/laptop/hp-probook-440-g6-7df56ea.html", \
+				"https://www.epey.com/laptop/hp-15-dw2009nt-3h813ea.html", \
+				"https://www.epey.com/laptop/monster-huma-h4-v3-1.html"] 
 
-laptop_names = ["HP ProBook 440 G6 (7DF56EA) Notebook", \
-                "HP Pavilion x360 14-dh1005nt (8XH01EA) 2'si 1 Arada", \
-                "Asus UM431DA-AM024T Ultrabook", \
-                "HP 14-ce3003nt (3H905EA) Notebook", \
-                "Acer Swift 3 SF314-58G-53HN Ultrabook (NX.HPKEY.002)", \
-                "Huawei MateBook 13 8+512 GB Notebook", \
-                "Huawei MateBook D 14 AMD Notebook (53010WPX)", \
-                "Lenovo S540 81ND003UTX Notebook", \
-                "HP 14-cf2005nt (9CP83EA) Notebook", \
-                "HP 340S G7 (10R29EA) Notebook", \
-                "HP 14-cf2004nt (9CN08EA) Notebook", \
-                "HP 14-cf1018nt (8BW26EA) Notebook", \
-                "Dell Inspiron 5490", \
-                "Monster Huma H4 V3.2"]
+product_names = ["HP ProBook 440 G6 (7DF56EA) Notebook", \
+				"HP 15-dw2009nt (3H813EA) Notebook", \
+				"Monster Huma H4 V3.1 Notebook"]
 
 #SetUp Email information. Body will be filled throughout the code
 msg = EmailMessage()
-msg['Subject'] = "Laptop Discount Information"
+msg['Subject'] = "Ürün İndirim Bilgisi"
 msg['From'] = "yourbotmail@gmail.com"
 msg['To'] = "yourownmail@gmail.com"
 messageBody = ""
 anyChanges = False  #If even there is a single discount, we'll set this to True (to send mail)
 
-#Opening min file and accessing all min values of laptops
+#Opening min file and accessing all min values of products
 f = open("min.txt", "r")
 lines = f.readlines()
 
-for i in range(len(laptop_names)):
-	laptopName = lines[i*2]
+for i in range(len(product_names)):
+	productName = lines[i*2]
 	oldPrice = float(lines[i*2 + 1])
 
 	#Getting new html object
-	r = requests.get(laptop_links[i])
+	r = requests.get(product_links[i])
 	html_doc = r.content
 	soup = BeautifulSoup(html_doc, 'html.parser')
 
-	#Accessing the first price
-	priceText = soup.find("span",attrs={"class":"urun_fiyat"}).text
+	try:
+		#Accessing the first price
+		priceText = soup.find("span",attrs={"class":"urun_fiyat"}).text
 
-	#Chopping string after TL
-	sep = 'TL'
-	onlyPrice = priceText.split(sep, 1)[0]
+		#Chopping string after TL
+		sep = 'TL'
+		onlyPrice = priceText.split(sep, 1)[0]
 
-	#Remove . from string
-	onlyPrice = onlyPrice.replace('.', '')
-	onlyPrice = onlyPrice.replace(',', '.')
+		#Remove . from string
+		onlyPrice = onlyPrice.replace('.', '')
+		onlyPrice = onlyPrice.replace(',', '.')
 
-	#Convert to Float
-	priceFloat = float(onlyPrice)
+		#Convert to Float
+		priceFloat = float(onlyPrice)
 
-	lines[i*2 + 1] = str(priceFloat) + '\n'
-	#If new price is lower, edit corresponding line
-	if priceFloat < oldPrice:
-		messageBody += laptop_names[i] + " Notebook Discount!\nOld Price = " + str(oldPrice) + "\nNew Price = " + \
-		              str(priceFloat) + "\n\n"
-		anyChanges = True
+		lines[i*2 + 1] = str(priceFloat) + '\n'
+		#If new price is lower, edit corresponding line
+		if priceFloat < oldPrice:
+			messageBody += product_names[i] + " Ürün İndirimde\nEski Fiyat = " + str(oldPrice) + "\nYeni Fiyat = " + \
+			              str(priceFloat) + "\n" + product_links[i] + "\n\n"
+			anyChanges = True
 
-	# and write everything back
-	with open('min.txt', 'w') as file:
-	    file.writelines( lines )
-	f.close()
-	
+		# and write everything back
+		with open('min.txt', 'w') as file:
+		    file.writelines( lines )
+		f.close()
+	except:
+		continue
 
 if anyChanges:
 	msg.set_content(messageBody)
